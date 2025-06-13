5954a0b2d0eedffe08fee0582e4c730e43d86268
CKBhalaji <bhalaji2003@gmail.com>
2025-06-13 12:11:48 +0530

    Learned till the part 88 from the Sanjeev Thiyagarajan Youtube channel

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,8 @@
 from typing import Optional
+from unittest import result
 from fastapi import FastAPI, status, Response, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-
+from sqlalchemy import func
 from app.routers import user
 from .. import models, schemas, oauth2
 from ..database import get_db
@@ -13,7 +14,8 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[schemas.Post])
+@router.get("/", response_model=list[schemas.PostOut])
+# @router.get("/")
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
@@ -23,7 +25,15 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     
     # posts = db.query(models.Post).all()
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # post_list = []
+    # for post, votes in results:
+    #     post_data = post.__dict__
+    #     post_data["votes"] = votes
+    #     post_list.append(post_data)
+    
     return posts
 
 
@@ -41,11 +51,12 @@ def create_posts(post:schemas.postCreate,db: Session = Depends(get_db), current_
     return new_post
 
     
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
     # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Post.id == models.Vote.post_id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,12 @@
 from calendar import c
 from datetime import datetime
-from typing import Optional
+from typing import Annotated, Optional
+from httpx import post
 from pydantic import BaseModel, EmailStr, conint
 
-
+class Config:
+    from_attributes = True
+    
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -17,20 +20,24 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
     
-class postBase(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
     
-class postCreate(postBase):
+class postCreate(PostBase):
     pass
 
-class Post(postBase):
+class Post(PostBase):
     id: int 
     created_at: datetime
     owner_id: int
     owner: UserOut
- 
+    
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+    
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -40,4 +47,4 @@ class TokenData(BaseModel):
     
 class Vote(BaseModel):
     post_id: int
-    dir: conint(le=1)
+    dir: Annotated[int, conint(ge=0, le=1)]
