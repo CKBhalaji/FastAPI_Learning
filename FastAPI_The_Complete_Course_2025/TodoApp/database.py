4429c288b871f2e0b0945c6d7b8a91dce94d657c
CKBhalaji <bhalaji2003@gmail.com>
2025-06-22 13:04:06 +0530

    Add for the database error

--- FastAPI_The_Complete_Course_2025/TodoApp/database.py
+++ FastAPI_The_Complete_Course_2025/TodoApp/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2003@localhost:5432/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2003@localhost:5432/TodoApplicationDatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
